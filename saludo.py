758a95c644dc57db8eafc4ff23f777b5d4f06cad
Ivan-Ocampo <ivanocampo.94@gmail.com>
2025-11-19 23:42:34 -0300

    Update 001: Se agrega el saludo de despedida

--- saludo.py
+++ saludo.py
@@ -3,5 +3,6 @@
 def main():
     print("¡Hola, gente bella de la TUP-FRCU, bienvenidos a Metodologías de Sistemas 1!")
     print("")
+    print("Espero que tengan una linda cursada. Saludos")
 if __name__ == "__main__":
     main()
